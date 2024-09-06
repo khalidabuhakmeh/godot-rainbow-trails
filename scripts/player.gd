0231f34d5f41c2f08e15d6ef3fd3f8d65f9319bf
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2024-09-06 12:49:44 -0400

    Enhance player physics
    Updated jump mechanics for better gameplay, including a customized gravity function for faster fall speeds and adjusted jump velocity.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 var initial_logo_scale: Vector2
 
@@ -12,12 +12,22 @@ var initial_logo_scale: Vector2
 func _ready() -> void:
 	initial_logo_scale = $Logo.scale
 
+# Fall faster than you jump
+# adapted from https://www.youtube.com/watch?v=bn3ZUCZ0vMo
+func get_custom_gravity():
+	if velocity.y < 0:
+		return get_gravity()
+	return get_gravity() * 1.5
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_custom_gravity() * delta
 		logo.scale.y = move_toward(logo.scale.y, initial_logo_scale.y,  0.01)
 		logo.scale.x = move_toward(logo.scale.x, initial_logo_scale.x,  0.01)
+		
+	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY / 4
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
